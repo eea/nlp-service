8195344bbddcb977741a6766df3502803f67efba
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-08-27 10:21:10 +0300

    Add command line arguments to the pipeline processor

--- app/commands/run_pipeline.py
+++ app/commands/run_pipeline.py
@@ -1,15 +1,13 @@
 from pathlib import Path
 
-from app.core.config import PIPELINE_YAML_PATH
-from haystack import Pipeline
-from haystack.document_store.elasticsearch import ElasticsearchDocumentStore
-from haystack.retriever.dense import DensePassageRetriever
-from loguru import logger
+import click
 
 
 def run_pipeline(pipeline_name, request):
     """ Run a pipeline from the command line
     """
+    from app.core.config import PIPELINE_YAML_PATH
+    from haystack import Pipeline
     pipeline = Pipeline.load_from_yaml(
         Path(PIPELINE_YAML_PATH), pipeline_name=pipeline_name
     )
@@ -17,11 +15,26 @@ def run_pipeline(pipeline_name, request):
     return result
 
 
-def dpr_processing_pipeline():
+@click.command()
+@click.argument('index')
+@click.argument('textfield')
+@click.argument('embeddingfield')
+@click.option('--host', default='localhost')
+@click.option('--port', default='9200')
+def dpr_processing_pipeline(index, textfield, embeddingfield, host, port):  # ,
+    from haystack.document_store.elasticsearch import \
+        ElasticsearchDocumentStore
+    from haystack.retriever.dense import DensePassageRetriever
+    from loguru import logger
     elastic = ElasticsearchDocumentStore(
-        index="global-search", create_index=False,
-        text_field="description",
-        embedding_field="description_embedding"
+        host=host,
+        port=port,
+        index=index,
+        create_index=False,
+        text_field=textfield,       # all_fields_for_freetext
+        embedding_field=embeddingfield,  # description_embedding
+        # index="global-search", create_index=False,
+        # text_field="description",
     )
     retriever = DensePassageRetriever(elastic)
     elastic.update_embeddings(retriever=retriever)
