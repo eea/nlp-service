15276d8bad1debe27a633125d342e55a26b71524
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-09-30 14:22:28 +0300

    Improve query classifier

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # poetry run uvicorn --host 0.0.0.0 app.main:app
 
-NLP_SERVICES=search,embedding TOKENIZERS_PARALLELISM=false uvicorn --workers=1 --host 0.0.0.0 app.main:app
+NLP_SERVICES=search,embedding,qa,question-classifier TOKENIZERS_PARALLELISM=false uvicorn --workers=1 --host 0.0.0.0 app.main:app
 #
 #NLP_SERVICES=question-classifier
