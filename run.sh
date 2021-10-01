7dc2b048a2bc8fd63f36dabe762e7c99ffb8af5e
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-10-01 11:28:14 +0300

    Rename Reader to AnswerExtraction

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # poetry run uvicorn --host 0.0.0.0 app.main:app
 
-NLP_SERVICES=qa TOKENIZERS_PARALLELISM=false uvicorn --workers=1 --host 0.0.0.0 app.main:app
-#,search,embedding,question-classifier
-#NLP_SERVICES=question-classifier
+NLP_SERVICES=qa,search,embedding,question-classifier,summarizer TOKENIZERS_PARALLELISM=false uvicorn --workers=1 --host 0.0.0.0 app.main:app
+#
