4060b5bc545123498abd9717536529b15c96c502
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-10-12 11:18:52 +0300

    Add spacy data.txt file

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # poetry run uvicorn --host 0.0.0.0 app.main:app
 
-NLP_SERVICES=qa,search,embedding,question-classifier,similarity TOKENIZERS_PARALLELISM=false uvicorn --workers=1 --host 0.0.0.0 app.main:app
+NLP_SERVICES=qa,search,embedding,question-classifier,question-generation,similarity,summarizer,spacy,zeroshot-classifier,ner TOKENIZERS_PARALLELISM=false uvicorn --workers=1 --host 0.0.0.0 app.main:app
 #,qa,,,summarizer
